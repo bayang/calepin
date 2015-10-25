3bde3eb36ccf342bfb2e068a5b19f94e9a2dc1bc
bayang <bayang@users.noreply.github.com>
2015-10-25 18:21:33 +0100

    added error checking

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask import Markup
 from markdown import markdown
 from peewee import *
 from pyembed.markdown import PyEmbedMarkdown
+from pyembed.core.consumer import PyEmbedConsumerError
 
 from app import db
 
@@ -17,8 +18,14 @@ class Note(Model):
         database = db
 
     def html(self):
-        html = markdown(self.content, extensions=[PyEmbedMarkdown()])
-        return Markup(html)
+        try:
+            html = markdown(self.content, extensions=[PyEmbedMarkdown()])
+            return Markup(html)
+        except PyEmbedConsumerError as e:
+            modif_note = self.content.replace(self.content[0:23], "<")
+            modif_note = modif_note.replace(self.content[-1], ">")
+            html = markdown(modif_note)
+            return Markup(html)
 
     @classmethod
     def public(cls):
